d5902e9f4fb5f46e286e370fece39eea49a8f95f
Itay Lazarov <lazarovitay@gmail.com>
2021-05-02 22:50:52 +0300

    I added a function that get from the DataBase the list of the names of the saved games.

--- DataBase Handler/DataBaseHandler.cs
+++ DataBase Handler/DataBaseHandler.cs
@@ -1,6 +1,9 @@
 ﻿using Checkers.Model;
 using Repository;
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Data;
 
 namespace Checkers.DataBase_Handler
 {
@@ -20,13 +23,23 @@ namespace Checkers.DataBase_Handler
             //Get The List Of The Board Tiles Of The Game (List<BoardTile>)
             var boardList = BoardTilesData.GetData(gameId);
 
-            if (boardList == null)
+            if (boardList.Count == 0)
             {
                 Console.WriteLine("The List is Null");
                 return null;
             }
 
             return ConvertBoardEntitiesToBoard.ConvertFromListToBoard(boardList);
+        }
+
+        public static List<string> GetTheListOfSavedGames()
+        { 
+            using(CheckersDBEntities db = new CheckersDBEntities())
+            {
+                var names = from Names in db.GameManagments
+                            select Names.Name;
+                return names.ToList();
+            }
         }
 
         public static bool InsertTheSavedGame(Board board, string nameOfTheSavedGame)
