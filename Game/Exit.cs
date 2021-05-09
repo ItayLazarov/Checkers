6153d33de6becca3a98e686c3aca8cacc3d958b2
Itay Lazarov <lazarovitay@gmail.com>
2021-05-09 13:48:42 +0300

    I added this class for better organization in my project

---
+++ Game/Exit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Checkers.Game
+{
+    public class Exit
+    {
+        public static bool ExitCheck()
+        {
+            if (SettingGame.BlackPawnsAlive.Count > 0 && SettingGame.WhitePawnsAlive.Count > 0)
+                return false;
+
+            else if (SettingGame.BlackPawnsAlive.Count == 0 && SettingGame.WhitePawnsAlive.Count == 0)
+                Console.WriteLine("\nThere is no Game Running at the moment...\n");
+
+            else if (SettingGame.BlackPawnsAlive.Count == 0)
+                Console.WriteLine("\nWhite Won!!\n");
+
+            else if (SettingGame.WhitePawnsAlive.Count == 0)
+                Console.WriteLine("\nBlack Won!!\n");
+
+            return true;
+        }
+
+        public static bool ExitGame()
+        {
+            while(true)
+            {
+                Console.WriteLine("Do You Want To Exit? (Y/N)");
+
+                var cki = Console.ReadKey(true);
+
+                if (cki.Key == ConsoleKey.Y)
+                    return true;
+
+                if (cki.Key == ConsoleKey.N)
+                    return false;
+                
+                else
+                    Console.WriteLine("Please Enter Y or N");
+            }
+        }
+    }
+}
